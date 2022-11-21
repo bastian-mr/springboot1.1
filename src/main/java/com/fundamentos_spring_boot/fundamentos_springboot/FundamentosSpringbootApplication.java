eb5050111ed2b2d3f8b64c4fd6cd81915230d7bf
bastian-mr <bastian.milla14@gmail.com>
2022-11-21 18:40:15 -0300

    first commit

---
+++ src/main/java/com/fundamentos_spring_boot/fundamentos_springboot/FundamentosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos_spring_boot.fundamentos_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos_spring_boot/fundamentos_springboot/FundamentosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos_spring_boot.fundamentos_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
